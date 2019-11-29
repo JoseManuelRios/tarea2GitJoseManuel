5f7beb899d0cd93de3e1277355ebd2141b4e8bd9
jriosaguilar <josemanueljbk99@gmail.com>
2019-11-29 13:37:19 +0100

    Creación del proyecto

---
+++ src/main/java/com/mycompany/tarea2gitjosemanuelrios/tarea2GitJoseManuelRios.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea2gitjosemanuelrios;
+
+/**
+ *
+ * @author jriosaguilar
+ */
+public class tarea2GitJoseManuelRios {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Jose Manuel");
+        
+    }
+    
+}
